d0203bd344087c2b7828d580f43a2ed0c04cc332
Ivansancor <ivanvaldeolivo95@gmail.com>
2022-01-01 23:00:06 +0100

    created and styled basic grid

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const div = document.querySelector('div');
+const parentdiv = document.getElementById('parent-div');
+
+for (let i = 0; i < 16; i++){
+    let newdiv = document.createElement('div');
+    newdiv.classList.add('grid-div');
+    parentdiv.appendChild(newdiv);        
+}
